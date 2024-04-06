db0dc95e4c2d2b117e4a560add0be68ade65b016
Landy510 <randy.chen@tpisoftware.com>
2024-04-06 23:23:31 +0800

    style: add required font family and icon

--- src/features/pages/index/Index.tsx
+++ src/features/pages/index/Index.tsx
@@ -1,7 +1,11 @@
 function Index() {
   return (
     <div className="container">
-      <h1 className="text-center">This is Index Page</h1>
+      <span className="material-icons md-24">face</span>
+      <h2>THis is a h2</h2>
+      <h1 className="text-center font-Raleway">This is Index Page</h1>
+      <p className="font-Open-Sans">This is a paragraph</p>
+      <p className="font-Roboto-Serif">This is a Paragraph with Roboto-Serif</p>
       <button type="button" className="btn-purple">Click Me</button>
     </div>
   );
