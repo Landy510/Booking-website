bc8b55b758f7aa08e4091ab37e7f00a3641b5803
outsourcing <outsourcing@DD_outsourcing001.cathaysite.com.tw>
2024-04-11 15:25:37 +0800

    chore: modify the fileName of Header component

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
 
-import Header from './header/Header';
+import Header from './Header/Header';
 import Footer from './Footer/Footer';
 
 export default Layout;
