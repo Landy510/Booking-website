aeaf0a51e33288cee918d48f8770b8054107a9d8
outsourcing <outsourcing@DD_outsourcing001.cathaysite.com.tw>
2024-04-11 15:23:34 +0800

    chore: modify the fileName of Footer.tsx

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
 
 import Header from './header/Header';
-import Footer from './footer/Footer';
+import Footer from './Footer/Footer';
 
 export default Layout;
 
